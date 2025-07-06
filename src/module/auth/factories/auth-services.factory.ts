3a6e3fa6f5878d923ba7d857602efcaced8405bc
Alefe Bispo <alefe.bispo@onesight.com.br>
2025-07-05 22:29:47 -0300

    feat: create user and auth features

---
+++ src/module/auth/factories/auth-services.factory.ts
@@ -0,0 +1,17 @@
+import UserRepository, {
+  CreateUserDTO,
+} from "@/module/user/repository/user.repository";
+import { login, LoginDTO } from "../services/login.service";
+import { registerUser } from "../services/register.service";
+
+export function makeRegisterService(userRepository: UserRepository) {
+  return async function registerUserWithRepo(data: CreateUserDTO) {
+    return registerUser(userRepository, data);
+  };
+}
+
+export function makeLoginService(userRepository: UserRepository) {
+  return async function loginWithRepo(data: LoginDTO) {
+    return login(userRepository, data);
+  };
+}
