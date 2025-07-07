567b80cbf8b5e8f402a7260252a8252d50ddccb6
Alefe Bispo <alefe.bispo@onesight.com.br>
2025-07-07 18:27:25 -0300

    feat: add profile route and tests

--- src/module/auth/factories/auth-services.factory.ts
+++ src/module/auth/factories/auth-services.factory.ts
@@ -3,6 +3,7 @@ import UserRepository, {
 } from "@/module/user/repository/user.repository";
 import { login, LoginDTO } from "../services/login.service";
 import { registerUser } from "../services/register.service";
+import getProfile from "../services/get-profile.service";
 
 export function makeRegisterService(userRepository: UserRepository) {
   return async function registerUserWithRepo(data: CreateUserDTO) {
@@ -13,5 +14,11 @@ export function makeRegisterService(userRepository: UserRepository) {
 export function makeLoginService(userRepository: UserRepository) {
   return async function loginWithRepo(data: LoginDTO) {
     return login(userRepository, data);
+  };
+}
+
+export function makeGetProfileService(userRepository: UserRepository) {
+  return async function getProfileWithRepo(id: string) {
+    return getProfile(userRepository, id);
   };
 }
