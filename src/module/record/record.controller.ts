572f2e549c4ee155ee4af0cb0fbec0f3887832ff
Alefe Bispo <alefe.bispo@onesight.com.br>
2025-07-08 17:59:02 -0300

    feat: ensure record routes are protected

--- src/module/record/record.controller.ts
+++ src/module/record/record.controller.ts
@@ -10,22 +10,26 @@ import {
 export default function recordController(recordRepository: RecordRepository) {
   return {
     async create(request: FastifyRequest, reply: FastifyReply) {
+      const userId = request.user.sign.sub;
+
       const body = createSchema.parse(request.body);
 
       const createRecordService = makeCreateRecordService(recordRepository);
 
-      await createRecordService(body);
+      await createRecordService({ ...body, userId });
 
       return reply
         .status(201)
         .send({ message: "Ponto registrado com sucesso." });
     },
     async findAll(request: FastifyRequest, reply: FastifyReply) {
+      const userId = request.user.sign.sub;
+
       const params = findAllSchema.parse(request.query);
 
       const listRecordsService = makeListRecordsService(recordRepository);
 
-      const data = await listRecordsService(params);
+      const data = await listRecordsService({ ...params, userId });
 
       return reply.status(200).send(data);
     },
